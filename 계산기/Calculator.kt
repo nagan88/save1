a179f831e6fe50ab3d25ef82e96316f0d02dd0b5
nagan88 <161535770+nagan88@users.noreply.github.com>
2024-03-12 15:57:03 +0900

    Create Calculator.kt

---
+++ 계산기/Calculator.kt
@@ -0,0 +1,7 @@
+package com.example.calculater_application_v2
+
+class Calculator(private val operator:AbstractOperation){
+
+    fun operate(num1:Int,num2:Int):Double{
+        return operator.operate(num1,num2)
+    }
