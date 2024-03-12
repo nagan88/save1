3e2f8a924836e3833c74553502894d1e9904e203
nagan88 <161535770+nagan88@users.noreply.github.com>
2024-03-12 16:02:55 +0900

    Create DivideOperation.kt

---
+++ 계산기/DivideOperation.kt
@@ -0,0 +1,14 @@
+package com.example.calculater_application_v2
+
+import kotlin.jvm.Throws
+
+class DivideOperation : AbstractOperation({
+
+    override fun operate(num1: Int, num2: Int): Double{
+        require(num2!=0){//require:예외처리 함수
+            ArithmeticException("Divide by zero")//ArithmeticException: 함수를 0으로 나누면 수학적 오류 발생
+        }
+        return (num1/num2).toDouble()//num1를 num2으로 나눈 것을 toDouble로 반환
+    }
+
+}
