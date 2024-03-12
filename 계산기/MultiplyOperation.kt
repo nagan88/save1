a11577fdde57dedb807bd33328e2a1b07f295dcd
nagan88 <161535770+nagan88@users.noreply.github.com>
2024-03-12 16:02:16 +0900

    Create MultiplyOperation.kt

---
+++ 계산기/MultiplyOperation.kt
@@ -0,0 +1,7 @@
+package com.example.calculater_application_v2
+
+class MultiplyOperation : AbstractOperation(){
+
+    override fun operate(num1: Int, num2: Int): Double =(num1*num2).toDouble()
+
+}
