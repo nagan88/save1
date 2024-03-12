c0e706f38a6997c391698624f6302337992e4cda
nagan88 <161535770+nagan88@users.noreply.github.com>
2024-03-12 15:59:20 +0900

    Create MinusOperation.kt

---
+++ 계산기/MinusOperation.kt
@@ -0,0 +1,7 @@
+package com.example.calculater_application_v2
+
+class MinusOperation: AbstractOperation()/*Calculator()*/{
+
+    override fun operate(num1: Int, num2: Int): Double =(num1-num2).toDouble()
+
+}
